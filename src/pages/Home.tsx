73bd0886cb3b80d342d33810ede0c8d761e89ce1
EduardoVila <eduardodevila1@hotmail.com>
2020-11-10 19:23:42 -0300

    connecting with OpenWeather api

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,40 @@
-import React from 'react';
-import { FiChevronRight, FiSun } from 'react-icons/fi'
+import React, { useState } from 'react';
+import { FiChevronRight, FiSun, FiCloudRain, FiCloud, FiCloudSnow } from 'react-icons/fi';
 
-import '../styles/pages/home.css'
+import weatherApi from '../services/weatherApi';
+import '../styles/pages/home.css';
+
+interface Weather {
+  temp: number,
+  main: string
+};
 
 function Home() {
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState<Weather>();
+
+  function handleWithFetchWeather () {
+    weatherApi.get(`weather?q=${city}&units=metric&APPID=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`).then( res => {
+      setWeather({
+        temp: Math.round(res.data.main.temp),
+        main: res.data.weather[0].main
+      });
+    });
+  };
+
+  function handleWithTemperature () {
+    if (weather && weather.temp > 24) return 'Warm'
+    else if (weather && weather.temp > 14) return 'Cool'
+    return 'Cold';
+  };
+
+  function handleWithWeatherIcon () {
+    if (weather?.main === 'Rain') return  <FiCloudRain size={150} color="#FFFFFF" />;
+    else if (weather?.main === 'Clear') return <FiSun size={150} color="#FFFFFF"/>
+    else if (weather?.main === 'Clouds') return <FiCloud size={150} color="#FFFFFF" />;
+    else if (weather?.main === 'Snow') return <FiCloudSnow size={150} color="#FFFFFF" />;
+  };
+
   return (
     <div id="page-home">
       <div className="content-wrapper">
@@ -16,8 +47,15 @@ function Home() {
 
           <div className="search-container">
             <h2>Search a city:</h2>
-            <input type="text" name="city" id="city" placeholder="Ex: Araranguá, SC, Brazil..."/>
-            <button>Search</button>
+            <input
+              type="text"
+              name="city"
+              id="city"
+              placeholder="Ex: Araranguá, SC, Brazil..."
+              value={city}
+              onChange={event => setCity(event.target.value)}
+            />
+            <button onClick={handleWithFetchWeather}>Search</button>
           </div>
 
           <div className="latest-searches-container">
@@ -32,12 +70,15 @@ function Home() {
         </aside>
 
         <main>
-          <img src="https://pix10.agoda.net/geo/city/17164/1_17164_02.jpg?s=1920x822" alt=""/>
-          <div className="content">
-            <FiSun size={150} color="#FFFFFF"/>
-            <p>32º</p>
-            <span>Sunny  |  Clear</span>
-          </div>
+          { weather &&  <>
+            <img src="https://pix10.agoda.net/geo/city/17164/1_17164_02.jpg?s=1920x822" alt=""/>
+            <div className="content">
+              { handleWithWeatherIcon() }
+
+              <p>{ weather?.temp }º</p>
+              <span>{ weather?.main }  |  { handleWithTemperature() }</span>
+            </div>
+          </> }
         </main>
       </div>
     </div>
